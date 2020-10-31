b6df07631a5c955b02bef2db9a2d6306b46d784c
daniel.alvarez <daniel.alvarez.m11@gmail.com>
2020-10-30 19:12:30 -0600

    Updates

---
+++ precache-manifest.fe06b4ede11af9220dd499b8e85d1ad4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "027c0ce391920a3ff328993e38ebc26e",
+    "url": "/shopping-cart-react/index.html"
+  },
+  {
+    "revision": "cf652aea2bbf70274d90",
+    "url": "/shopping-cart-react/static/css/main.efc419f6.chunk.css"
+  },
+  {
+    "revision": "b2091a98179952ce972d",
+    "url": "/shopping-cart-react/static/js/2.e8c4ded1.chunk.js"
+  },
+  {
+    "revision": "5f2d6de39c39f8b6e46315c29574aa22",
+    "url": "/shopping-cart-react/static/js/2.e8c4ded1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf652aea2bbf70274d90",
+    "url": "/shopping-cart-react/static/js/main.05ef88e0.chunk.js"
+  },
+  {
+    "revision": "78bc448bd1de6204f935",
+    "url": "/shopping-cart-react/static/js/runtime-main.9b0b16a8.js"
+  }
+]);
